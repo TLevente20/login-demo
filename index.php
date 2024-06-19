8861109b2d578035686d39a1460d62a91b9a05d9
TLevente20 <takacs.levente1015@gmail.com>
2024-06-19 13:15:26 +0200

    Inicial database setup

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ use App\App;
 
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/routes/web.php';
+require __DIR__ . '/config/database.php';
 
 // Using App class method to start loggers
 App::runLoggers();
-
