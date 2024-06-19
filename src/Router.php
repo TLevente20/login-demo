ae381b6d25f1fe6e72ec22c52f4197aede9adf64
TLevente20 <takacs.levente1015@gmail.com>
2024-06-20 01:15:26 +0200

    Remove debugging

--- src/Router.php
+++ src/Router.php
@@ -22,14 +22,12 @@ class Router
         $route = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         
         if (isset(self::$routes[$method][$route])) {
-            error_log("route is found");
             $callback = self::$routes[$method][$route];
             $request = new Request($_REQUEST); 
             $response = new Response();
             $callback($request, $response);
         } else {
             // Handle 404 Not Found
-            error_log("route is not found");
             http_response_code(404);
             echo "404 Not Found";
         }
